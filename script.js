3fc6f8fccdff04563ddaffa4c4b248193fb00266
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-11-17 02:34:28 -0300

    Subo semana 46 y 47

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
    // Datos para el gráfico de líneas
 const dataLine1 = {
-  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 44',
-    data: [15, 21, 9, 16, 22], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 46',
+    data: [7, 6, 80], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 45',
-    data: [9, 10, 7, 7, 11], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 47',
+    data: [15, 17, 12, 9, 23], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
