2e4da140b500bb48e9da3e8ff57cd7aeaaf2bf92
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-01-25 13:36:10 -0300

    Modify km data and video element setup
    Updated km data for Semana 4 and adjusted video element creation.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días],
   datasets: [{
     label: 'Km recorridos en Semana 4',
-    data: [10, 4, 13, 16, 25], // Datos de los km recorridos en cada día
+    data: [10, 15, 10, 16, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -108,4 +108,5 @@ function actualizarDiasRestantes() {
   });
 
 });
+
 
