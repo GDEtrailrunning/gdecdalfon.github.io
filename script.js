f88fb5ff1f704e30477ba592f24f02fb721ae95e
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-03-22 12:48:17 -0300

    Subo semana 12 y 13

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //actualizarDiasRestantes();
-  //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+  actualizarDiasRestantes();
+  setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
    // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'], // Etiquetas de los días],
   datasets: [{
-    label: 'Km recorridos en Semana 10',
-    data: [15, 19, 23, 18, 20], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 12',
+    data: [19, 21, 11, 10, 16], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 11',
-    data: [10, 18, 29, 16, 27], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 13',
+    data: [9, 9, 8, 8, 12], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
